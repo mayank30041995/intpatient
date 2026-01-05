dc9f117246797bf3db15b0b45f8b79fa7b685fe6
Mayank Sharma <mayankece155@gmail.com>
2026-01-05 18:04:09 +0530

    sanjeevani

---
+++ sanjeevani-hospital/js/set-json-global.js
@@ -0,0 +1,1 @@
+"use strict";(()=>{(()=>{let r=document.getElementById("jsonFormatterRaw")?.querySelector("pre");r&&setTimeout(()=>{try{let e=JSON.parse(r.innerText);Object.defineProperty(window,"json",{value:e,configurable:!0,enumerable:!1,writable:!1}),console.log('JSON Formatter: Type "json" to inspect')}catch(e){console.error("JSON Formatter: Failed to expose JSON global",e)}},120)})();})();
