4d53f87e93af1917dc44c58e3632a7116616fb1e
Mayank Sharma <mayankece155@gmail.com>
2026-02-04 11:44:09 +0530

    sanjeevani features

--- sanjeevani-app-v3/public/lib/index.js
+++ sanjeevani-app-v3/public/lib/index.js
@@ -112,7 +112,7 @@ const doctors = [
                             <i class="fa-solid fa-leaf text-green-500 mt-1"></i>
                             <div>
                                 <span class="font-semibold text-white">Other Areas</span><br>
-                                Naturopathic Physician
+                                Physiotherapist Physician
                             </div>
                         </li>
 
@@ -120,7 +120,7 @@ const doctors = [
                             <i class="fa-solid fa-graduation-cap text-green-500 mt-1"></i>
                             <div>
                                 <span class="font-semibold text-white">Education</span><br>
-                                25 Degrees, Jaipur University (1996)
+                                MPT
                             </div>
                         </li>
 
@@ -128,7 +128,7 @@ const doctors = [
                             <i class="fa-solid fa-briefcase text-green-500 mt-1"></i>
                             <div>
                                 <span class="font-semibold text-white">Experience</span><br>
-                                CMHO at Sanjeevani Kaya Shodhan Sansthan
+                                MPT at Sanjeevani Kaya Shodhan Sansthan
                             </div>
                         </li>
 
@@ -182,7 +182,7 @@ const doctors = [
                             <i class="fa-solid fa-graduation-cap text-green-500 mt-1"></i>
                             <div>
                                 <span class="font-semibold text-white">Education</span><br>
-                                25 Degrees, Jaipur University (1996)
+                               DNYS
                             </div>
                         </li>
 
@@ -190,7 +190,180 @@ const doctors = [
                             <i class="fa-solid fa-briefcase text-green-500 mt-1"></i>
                             <div>
                                 <span class="font-semibold text-white">Experience</span><br>
-                                CMHO at Sanjeevani Kaya Shodhan Sansthan
+                                DNYS at Sanjeevani Kaya Shodhan Sansthan
+                            </div>
+                        </li>
+
+                    </ul>`,
+  },
+    {
+    id: '4',
+    name: 'Dr. Reeta Majhi',
+    slug: 'reeta-majhi',
+    specialty: 'naturopathic specialist',
+    experience: '10 years',
+    image: '/images/doctors/dr-reeta-majhi.webp',
+    meta: {
+      title: 'Dr. Amit Sharma - Sanjeevani Kaya Hospital',
+      description:
+        'Dr. Reeta Majhi is a Naturopathic Doctor at Sanjeevani Kaya Shodan Sansthan, Panipat, Haryana, India.',
+      keywords:
+        'Sanjeevani Kaya Hospital doctors, naturopathic specialists, holistic doctors, natural healing experts, naturopathic physician team',
+    },
+    description: `<p class="text-lg text-gray-300 leading-relaxed mb-6">
+                      Dr. Reeta Majhi is a Naturopathic Doctor at Sanjeevani Kaya Shodan Sansthan, Panipat, Haryana, India.
+                    </p>
+
+                    <p class="text-gray-400 leading-relaxed mb-4">
+                       She practices holistic healing through naturopathy, focusing on natural therapies, lifestyle correction, detoxification, diet management, and mind–body balance. With a compassionate and patient-centered approach, she supports individuals in managing lifestyle disorders and improving overall well-being by addressing the root cause of illness rather than just symptoms.
+                    </p>`,
+    info: `   <ul class="space-y-5 text-gray-300">
+
+                        <li class="flex gap-3">
+                            <i class="fa-solid fa-stethoscope text-green-500 mt-1"></i>
+                            <div>
+                                <span class="font-semibold text-white">Speciality</span><br>
+                                Naturopathic specialist
+                            </div>
+                        </li>
+
+                        <li class="flex gap-3">
+                            <i class="fa-solid fa-leaf text-green-500 mt-1"></i>
+                            <div>
+                                <span class="font-semibold text-white">Other Areas</span><br>
+                               Detoxification
+                            </div>
+                        </li>
+
+                        <li class="flex gap-3">
+                            <i class="fa-solid fa-graduation-cap text-green-500 mt-1"></i>
+                            <div>
+                                <span class="font-semibold text-white">Education</span><br>
+                              PHD
+                            </div>
+                        </li>
+
+                        <li class="flex gap-3">
+                            <i class="fa-solid fa-briefcase text-green-500 mt-1"></i>
+                            <div>
+                                <span class="font-semibold text-white">Experience</span><br>
+                                Naturopathic specialist at Sanjeevani Kaya Shodhan Sansthan
+                            </div>
+                        </li>
+
+                    </ul>`,
+  },
+    {
+    id: '5',
+    name: 'Dr. Yogender Mishra',
+    slug: 'yogender-mishra',
+    specialty: 'yogacharya',
+    experience: '10 years',
+    image: '/images/doctors/dr-yogender-mishra.webp',
+    meta: {
+      title: 'Dr. Yogender Mishra - Sanjeevani Kaya Hospital',
+      description:
+        'Dr. Yogender Mishra is a highly respected Yogacharya and holistic wellness expert with years of experience in therapeutic yoga, preventive healthcare',
+      keywords:
+        'Sanjeevani Kaya Hospital doctors, yogacharya, yogacharya doctors, natural healing experts, acupressure physician team',
+    },
+    description: `<p class="text-lg text-gray-300 leading-relaxed mb-6">
+                        Dr. Yogender Mishra is an yogacharya specialist practicing at Sanjeevani Kaya Shodhan Sansthan in
+                        Gohana, Haryana.
+                    </p>
+
+                    <p class="text-gray-400 leading-relaxed mb-4">
+                       Dr. Yogender Mishra is a highly respected Yogacharya and holistic wellness expert with years of experience in therapeutic yoga, preventive healthcare, and mind–body healing. He is dedicated to helping individuals achieve physical fitness, mental clarity, and emotional balance through authentic yogic practices rooted in ancient Indian traditions and modern wellness science.
+                   With a deep understanding of Yoga Therapy, Pranayama, Meditation, and Lifestyle Management, Dr. Mishra designs personalized yoga programs to address chronic health conditions, stress, and overall well-being.
+                       </p>`,
+    info: `   <ul class="space-y-5 text-gray-300">
+
+                        <li class="flex gap-3">
+                            <i class="fa-solid fa-stethoscope text-green-500 mt-1"></i>
+                            <div>
+                                <span class="font-semibold text-white">Speciality</span><br>
+                               yogacharya
+                            </div>
+                        </li>
+
+                        <li class="flex gap-3">
+                            <i class="fa-solid fa-leaf text-green-500 mt-1"></i>
+                            <div>
+                                <span class="font-semibold text-white">Other Areas</span><br>
+                               Yoga Therapy & Naturopathy
+                            </div>
+                        </li>
+
+                        <li class="flex gap-3">
+                            <i class="fa-solid fa-graduation-cap text-green-500 mt-1"></i>
+                            <div>
+                                <span class="font-semibold text-white">Education</span><br>
+                                Master’s Degree in Yoga Science
+                            </div>
+                        </li>
+
+                        <li class="flex gap-3">
+                            <i class="fa-solid fa-briefcase text-green-500 mt-1"></i>
+                            <div>
+                                <span class="font-semibold text-white">Experience</span><br>
+                               Yoga therapy sessions, workshops, and wellness retreats
+                            </div>
+                        </li>
+
+                    </ul>`,
+  },
+    {
+    id: '6',
+    name: 'Dr. Nitin Dhruv',
+    slug: 'nitin-dhruv',
+    specialty: 'internal medicine',
+    experience: '10 years',
+    image: '/images/doctors/dr-nitin-dhruv.webp',
+    meta: {
+      title: 'Dr. Amit Sharma - Sanjeevani Kaya Hospital',
+      description:
+        'Meet the experienced Internal Medicine doctor DR. Amit Sharma at Sanjeevani Kaya Hospital, dedicated to personalized, natural healing through holistic and drug-free treatment approaches.',
+      keywords:
+        'Sanjeevani Kaya Hospital doctors, Internal Medicine specialists, holistic doctors, natural healing experts, Internal Medicine physician team',
+    },
+    description: `<p class="text-lg text-gray-300 leading-relaxed mb-6">
+                        Dr. Nitin Dhruv, a dedicated practitioner of Evidence-Based Ayurveda since 2018, specializes in Ayurvedic Internal Medicine (Kayachikitsa) and Panchkarma therapies. 
+                    </p>
+
+                    <p class="text-gray-400 leading-relaxed mb-4">
+                      Proficient in Agnikarma procedures, he has successfully managed diverse conditions, particularly excelling in Musculo-skeletal diseases affecting bones, joints, muscles and connective tissues. Additionally, Dr. Dhruv is well-versed in addressing Neurological and Neuro-degenerative diseases (Vata-Vyadhi) such as Alzheimer’s and Parkinson’s disease, as well as Paralysis. His expertise extends to treating Lifestyle disorders including Heart disease, Atherosclerosis, Stroke, Respiratory ailments, Obesity and Type 2 diabetes and skin diseases. Dr. Dhruv brings a wealth of knowledge and experience to provide comprehensive care for his patients.
+                    </p>`,
+    info: `   <ul class="space-y-5 text-gray-300">
+
+                        <li class="flex gap-3">
+                            <i class="fa-solid fa-stethoscope text-green-500 mt-1"></i>
+                            <div>
+                                <span class="font-semibold text-white">Speciality</span><br>
+                               Internal Medicine
+                            </div>
+                        </li>
+
+                        <li class="flex gap-3">
+                            <i class="fa-solid fa-leaf text-green-500 mt-1"></i>
+                            <div>
+                                <span class="font-semibold text-white">Other Areas</span><br>
+                                Natural pain relief treatments
+                            </div>
+                        </li>
+
+                        <li class="flex gap-3">
+                            <i class="fa-solid fa-graduation-cap text-green-500 mt-1"></i>
+                            <div>
+                                <span class="font-semibold text-white">Education</span><br>
+                                PhD in Kaya chikitsaSwami Narayan university,Kalol, Ahemdabad,Gujarat
+                            </div>
+                        </li>
+
+                        <li class="flex gap-3">
+                            <i class="fa-solid fa-briefcase text-green-500 mt-1"></i>
+                            <div>
+                                <span class="font-semibold text-white">Experience</span><br>
+                                 6+ Years of Experience - PhD in Kaya ChikitsaSwami Narayan university,Kalol, Ahemdabad,Gujarat
                             </div>
                         </li>
 
--- sanjeevani-app-v3/public/lib/treatment.js
+++ sanjeevani-app-v3/public/lib/treatment.js
@@ -1,13 +1,13 @@
 // Fake database (replace with real DB later)
 const treatments = [
-  {
+    {
     id: '1',
     title: 'naturopathy',
     slug: 'naturopathy',
     description1:
-      'Naturopathy recognizes that the human body is not a passive structure but a living, intelligent system guided by a vital life force. This life force—often referred to as vital energy—is responsible for all essential functions of the body, including breathing, digestion, circulation, metabolism, movement, brain activity, and emotional balance.',
+      'At Sanjeevani India Hospital, naturopathy is practiced as a science of natural healing that restores the body’s innate ability to heal itself.',
     description2:
-      'This vital energy is endowed at the time of conception. While it cannot be artificially created, it can be preserved, protected, and used efficiently to restore health and vitality. Disease, in the naturopathic view, does not arise due to the absence of medicine, but due to misuse, blockage, or depletion of this vital energy caused by unhealthy lifestyle choices, stress, improper diet, emotional strain, and disconnection from natural laws.',
+      'Our treatments focus on detoxification, nutrition therapy, yoga, hydrotherapy, mud therapy, and lifestyle correction under the guidance of highly experienced doctors. Set within a vast natural campus in Gohana, Sanjeevani offers ethical, affordable, and personalized naturopathy care for chronic diseases, lifestyle disorders, and overall wellness.',
     meta: {
       title:
         'Sanjeevani Kaya Hospital | Naturopathy Treatments & Natural Healing',
@@ -19,10 +19,82 @@ const treatments = [
     content: `../components/naturopathy.ejs`,
   },
   {
+    id: '1.0',
+    title: 'Naturopathy Treatment for Hydrotherapy',
+    slug: 'naturopathy-treatment-for-hydrotherapy-in-gohana',
+    description1:
+      'Hydrotherapy* is a natural therapeutic method that uses *water in different forms, temperatures, and applications* to promote healing, relieve pain, and improve overall health. It is an important component of *naturopathy and holistic healthcare',
+    description2:
+      'At Sanjeevani, hydrotherapy is used as a non-invasive, drug-free supportive treatment* to help the body heal naturally by stimulating circulation, relaxing muscles, and calming the nervous system.',
+    meta: {
+      title:
+        `Naturopathy Treatment for Hydrotherapy in Gohana | Sanjeevani Kaya Hospital Naturopathy Treatments & Natural Healing`,
+      description:
+        `Hydrotherapy is influenced by lifestyle, diet, and stress. At Sanjeevani, naturopathy focuses on addressing root causes through personalised, drug-free healing guided by doctors with over 20 years of average experience.`,
+      keywords:
+        'Sanjeevani Kaya Hospital, Naturopathy, natural healing, hydrotherapy, mud therapy, sun therapy, massage therapy, detox treatments, holistic wellness',
+    },
+    content: `../components/naturopathy/hydrotherapy.ejs`,
+  },
+  {
+    id: '1.1',
+    title: 'Naturopathy Treatment for Mud Therapy',
+    slug: 'naturopathy-treatment-for-mud-therapy-in-gohana',
+    description1:
+      'Mud therapy* is a natural therapeutic method in which *clean, natural earth (mud)* is applied externally to specific parts of the body or the whole body for healing and wellness purposes. It is an important treatment modality in *naturopathy and holistic healthcare.',
+    description2:
+      'At Sanjeevani, mud therapy is used as a **non-invasive, drug-free supportive treatment* to reduce inflammation, relieve pain, detoxify the body, and promote relaxation.',
+    meta: {
+      title:
+        `Naturopathy Treatment for mud-therapy in Gohana | Sanjeevani Kaya Hospital Naturopathy Treatments & Natural Healing`,
+      description:
+        `mud-therapy is influenced by lifestyle, diet, and stress. At Sanjeevani, naturopathy focuses on addressing root causes through personalised, drug-free healing guided by doctors with over 20 years of average experience.`,
+      keywords:
+        'Sanjeevani Kaya Hospital, Naturopathy, natural healing, diet-therapy, mud therapy, sun therapy, massage therapy, detox treatments, holistic wellness',
+    },
+    content: `../components/naturopathy/mudtherapy.ejs`,
+  },
+  {
+    id: '1.2',
+    title: 'Naturopathy Treatment for Diet Therapy',
+    slug: 'naturopathy-treatment-for-diet-therapy-in-gohana',
+    description1:
+      'Diet therapy is a therapeutic approach that uses planned, balanced, and natural food to support healing, manage diseases, and promote overall health. It is a core component of naturopathy and holistic healthcare, where food is considered a primary tool for prevention and recovery.',
+    description2:
+      'At Sanjeevani, diet therapy is used as a personalized, supportive treatment, tailored to individual health conditions, lifestyle, and nutritional needs.',
+    meta: {
+      title:
+        `Naturopathy Treatment for diet-therapy in Gohana | Sanjeevani Kaya Hospital Naturopathy Treatments & Natural Healing`,
+      description:
+        `diet-therapy is influenced by lifestyle, diet, and stress. At Sanjeevani, naturopathy focuses on addressing root causes through personalised, drug-free healing guided by doctors with over 20 years of average experience.`,
+      keywords:
+        'Sanjeevani Kaya Hospital, Naturopathy, natural healing, diet-therapy, mud therapy, sun therapy, massage therapy, detox treatments, holistic wellness',
+    },
+    content: `../components/naturopathy/diettherapy.ejs`,
+  },
+  {
+    id: '1.3',
+    title: 'Naturopathy Treatment for Massage Therapy',
+    slug: 'naturopathy-treatment-for-massage-therapy-in-gohana',
+    description1:
+      'Massage therapy is a therapeutic technique that involves *systematic manipulation of the soft tissues* of the body using hands, fingers, palms, or specialized techniques to promote relaxation, relieve pain, and improve overall health. It is an important component of *naturopathy and integrative healthcare*.',
+    description2:
+      'At *Sanjeevani, massage therapy is used as a **non-invasive, supportive treatment* to enhance circulation, reduce stress, relieve muscular tension, and support natural healing.',
+    meta: {
+      title:
+        `Naturopathy Treatment for massage therapy in Gohana | Sanjeevani Kaya Hospital Naturopathy Treatments & Natural Healing`,
+      description:
+        `Massage therapy is influenced by lifestyle, diet, and stress. At Sanjeevani, naturopathy focuses on addressing root causes through personalised, drug-free healing guided by doctors with over 20 years of average experience.`,
+      keywords:
+        'Sanjeevani Kaya Hospital, Naturopathy, natural healing, mud-therapy, mud therapy, sun therapy, massage therapy, detox treatments, holistic wellness',
+    },
+    content: `../components/naturopathy/massagetherapy.ejs`,
+  },
+  {
     id: '2',
     title: 'Ayurveda',
     slug: 'ayurveda',
-    description:
+    description1:
       'Ayurveda is the ancient Indian system of holistic healing that restores balance between body, mind, and spirit through natural therapies and lifestyle practices.',
     meta: {
       title:
@@ -38,7 +110,7 @@ const treatments = [
     id: '3',
     title: 'Acupressure',
     slug: 'acupressure',
-    description:
+    description1:
       'Acupressure is a natural healing technique based on stimulation of specific energy points on the body.',
     meta: {
       title:
@@ -54,7 +126,7 @@ const treatments = [
     id: '4',
     title: 'Physiotherapy',
     slug: 'physiotherapy',
-    description:
+    description1:
       'The Physiotherapy Unit at Sanjeevini India offers evidence-based rehabilitation and pain management therapies to restore mobility, strength, and functional independence.',
     meta: {
       title:
@@ -70,7 +142,7 @@ const treatments = [
     id: '5',
     title: 'Yoga & Pranayama',
     slug: 'yoga-pranayama',
-    description:
+    description1:
       'Yoga and Pranayama form a vital foundation of holistic wellness, enhancing physical strength, mental clarity, emotional balance, and inner harmony.',
     meta: {
       title:
@@ -84,9 +156,9 @@ const treatments = [
   },
   {
     id: '6',
-    title: 'Hydrotherapy and Massage units ',
+    title: 'Hydrotherapy and Massage ',
     slug: 'hydrotherapy',
-    description: 'Pancha Tatva Healing Unit',
+    description1: 'Pancha Tatva Healing',
     meta: {
       title:
         'Yoga & Pranayama in India | Sanjeevini India Holistic Wellness Center',
@@ -97,6 +169,22 @@ const treatments = [
     },
     content: `../components/hydrotherapy.ejs`,
   },
+  {
+    id: '7',
+    title: 'Personalized Integrated Treatment Plan',
+    slug: 'personalized-treatment-plan',
+    description1: 'Pancha Tatva Healing',
+    meta: {
+      title:
+        'Personalized Integrated Treatment Plan | Sanjeevini India Holistic Wellness Center',
+      description:
+        'Personalized Integrated Treatment Plan at Sanjeevini India. Our guided sessions improve flexibility, breathing, stress management, mental focus, and overall mind-body balance.',
+      keywords:
+        'Sanjeevini India, Yoga Therapy India, Pranayama Practice, Holistic Wellness, Stress Management Yoga, Breathing Exercises, Meditation Yoga, Mind Body Balance, Traditional Yoga Therapy',
+    },
+    content: `../components/personalizeplan.ejs`,
+  },
+  
 ]
 
 export { treatments }
