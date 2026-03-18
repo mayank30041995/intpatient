62954e4d75994ba78a1443319de2e8ccdf5f4aed
Mayank Sharma <mayankece155@gmail.com>
2026-03-18 13:57:16 +0530

    landing pages all

--- sanjeevani-app-v3/public/js/validation.js
+++
@@ -1,9 +0,0 @@
-function isValidEmail(email) {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
-}
-
-function sanitize(text = '') {
-  return text.replace(/[<>]/g, '').trim()
-}
-
-export { isValidEmail, sanitize }
